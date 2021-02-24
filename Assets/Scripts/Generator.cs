a80d0167944a2f5169395da29aa04f8b946c71a5
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2021-02-23 22:20:09 -0300

    refactor: add reference to generated tiles

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -6,33 +6,43 @@ public class Generator : MonoBehaviour
 
     private const int WIDTH = 100;
     private const int HEIGHT = 100;
-    
+
+    private GameObject[][] _map;
 
     [SerializeField] private GameObject[] _chunkModels;
 
     private void Awake()
     {
+        var time = Time.deltaTime;
+        _map = new GameObject[WIDTH][];
+        for (int i = 0; i < WIDTH; i++)
+        {
+            _map[i] = new GameObject[HEIGHT];
+        }
+
         for (int z = -(HEIGHT / 2); z < HEIGHT / 2; z++)
         {
+            int zWithoutOffset = z + HEIGHT / 2;
             for (int x = -(WIDTH / 2); x < WIDTH / 2; x++)
             {
                 int xWithoutOffset = x + WIDTH / 2;
-                int zWithoutOffset = z + HEIGHT / 2;
                 int r = GetFromNoise(xWithoutOffset, zWithoutOffset);
-                Instantiate(_chunkModels[r], new Vector3(x, 0, z), Quaternion.identity);
+                _map[xWithoutOffset][zWithoutOffset] = Instantiate(_chunkModels[r], new Vector3(x, 0, z), Quaternion.identity);
             }
         }
+
+        var concluded = Time.deltaTime;
+        Debug.Log(concluded - time);
     }
 
-    private int GetFromNoise(float x, float z) {
+    private int GetFromNoise(float x, float z)
+    {
         float xCoord = x * 10 / WIDTH;
         float zCoord = z * 10 / HEIGHT;
 
         float noiseFromPosition = Mathf.PerlinNoise(xCoord, zCoord);
 
         float remaped = Remap(noiseFromPosition, 0.0f, 1.0f, 0.0f, (float)_chunkModels.Length);
-        
-        Debug.Log(remaped);
 
         return (int)Math.Floor(remaped);
     }
